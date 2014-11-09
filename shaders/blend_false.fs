eea31881ef8ea39c6b50bd2cf751471d67f8fe33
admin <paddywwoof>
2014-11-09 22:06:47 +0000

    correction to blend_false

--- shaders/blend_false.fs
+++ shaders/blend_false.fs
@@ -38,7 +38,7 @@ void main(void) {
     texb = texture2D(tex1, coordsc);
   }
   vec4 texm = vec4(texf.b, 1.0 - texb.r, max(1.0 - texf.g, texb.g), 1.0);
-  gl_FragColor = mix(texf, mix(texb, texm, clamp(1.0 - tm * 2.0, 0.0, 1.0)), clamp(tm * 2.0, 0.0, 1.0));
+  gl_FragColor = mix(texf, mix(texb, texm, clamp(2.0 - tm * 2.0, 0.0, 1.0)), clamp(tm * 2.0, 0.0, 1.0));
   gl_FragColor.a *= unif[5][2];
 }
 

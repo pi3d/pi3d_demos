726620d8d3eeaf5e9e4296833502151ec6436ea2
admin <paddywwoof>
2014-11-18 10:12:27 +0000

    some tidying and clearer documentation

--- ClothWalk.py
+++ ClothWalk.py
@@ -13,21 +13,17 @@ from PIL import Image
 # cloth generation section
 ########################################################################
 def gcd(a, b):
-    """Return greatest common divisor using Euclid's Algorithm."""
-    while b:      
-        a, b = b, a % b
-    return a
+  """Return greatest common divisor using Euclid's Algorithm."""
+  while b:
+    a, b = b, a % b
+  return a
 
 def lcm(a, b):
-    """Return lowest common multiple."""
-    return a * b // gcd(a, b)
+  """Return lowest common multiple."""
+  return a * b // gcd(a, b)
 
-def lcmm(*args):
-    """Return lcm of args."""   
-    return reduce(lcm, args)
-
-PPM = 1800 # picks per m (aka weft threads)
-EPM = 1800 # ends per m (aka warp threads)
+EPM = 3200 # ends per gpu unit (aka warp threads)
+PPM = 3200 # picks per gpu unit (aka weft threads)
 yarn_colours = np.array([ # RGB values 0-255
 [50, 10, 20],
 [40, 55, 20],
@@ -39,33 +35,36 @@ yarn_colours = np.array([ # RGB values 0-255
 # yarn plan numbers refer to yarn_colours index (starting at 0 obviously)
 warp_plan = np.array(([3]*8 + [3,0,2]*2 + [4,4])*3 + [0]*7 + [5])
 weft_plan = np.array(([2]*8 + [1,5,4]*2 + [6,5])*3 + [0,1]*3 + [0,5])
-# heald, heddle, shaft, frame: the set of wires attached to a liftable frame
+# draft aka heald, heddle, shaft, frame: the set of wires attached to a liftable frame
 # position in the array corresponds to warp thread number is the 'column' of the peg_plan
 draft_plan = np.array([0,1,2,3, 0,1,2,3, 1,0,3,2, 1,0,3,2]) # simple herringbone
-# rows are weft insertions, column are shafts, 0 is down 1 is up
+# peg or lifting plan, rows are weft insertions, column are shafts, 0 is down 1 is up
 peg_plan = np.array([
 [0,1,1,0],
 [1,1,0,0],
 [1,0,0,1],
 [0,0,1,1]])
 
-pick_rpt = peg_plan.shape[0]
-draft_rpt = draft_plan.size
+pick_rpt = peg_plan.shape[0] # length in 1st dimension of array
+draft_rpt = draft_plan.size # length of array
 weft_rpt = weft_plan.size
 warp_rpt = warp_plan.size
-wv_width = lcm(warp_rpt, draft_rpt) # lowest common multiple
+
+# find the size of the sample
+wv_width = lcm(warp_rpt, draft_rpt) # lowest common multiple of weave and yarn patterns
 wv_height = lcm(weft_rpt, pick_rpt)
 
+# extend each of the plans to the size of the sample
 warp_plan = np.tile(warp_plan, (wv_width // warp_rpt))
 weft_plan = weft_plan.reshape((weft_rpt, 1)) # make into vertical array
 weft_plan = np.tile(weft_plan, (wv_height // weft_rpt, 1))
 draft_plan = np.tile(draft_plan, (wv_width // draft_rpt))
 peg_plan = np.tile(peg_plan, (wv_height // pick_rpt, 1))
 
-lifts = peg_plan[:,draft_plan[:]] # generate an up/down map for the plan
+lifts = peg_plan[:,draft_plan] # generate an up/down map for the plan
 # then fill it with the yarn for warp if it's up or weft if it's down
-yarn_plan = lifts[:,:] * warp_plan[:] + (lifts * -1 + 1)[:,:] * weft_plan[:]
-wv = yarn_colours[yarn_plan[:,:]].astype(np.uint8) # wv takes the [R,G,B] from yarn colours
+yarn_plan = lifts * warp_plan + (lifts * -1 + 1) * weft_plan
+wv = yarn_colours[yarn_plan].astype(np.uint8) # wv takes the [R,G,B] from yarn colours
 
 im = Image.fromarray(wv, 'RGB')
 clothimg = pi3d.Texture(im)
@@ -75,7 +74,7 @@ clothimg = pi3d.Texture(im)
 DISPLAY = pi3d.Display.create(x=100, y=100)
 DISPLAY.set_background(0.4, 0.8, 0.8, 1.0)      # r,g,b,alpha
 # yellowish directional light blueish ambient light
-pi3d.Light(lightpos=(1, -1, -3), lightcol =(1.0, 1.0, 0.8), lightamb=(0.25, 0.2, 0.3))
+pi3d.Light(lightpos=(1, -1, -3), lightcol =(1.0, 1.0, 0.8), lightamb=(0.45, 0.5, 0.6))
 
 #========================================
 
@@ -96,7 +95,6 @@ ectex=pi3d.loadECfiles("textures/ecubes","skybox_hall")
 myecube = pi3d.EnvironmentCube(size=900.0, maptype="FACES", name="cube")
 myecube.set_draw_details(flatsh, ectex)
 
-
 # Create elevation map
 mapwidth = 1000.0
 mapdepth = 1000.0
@@ -105,7 +103,7 @@ mountimg1 = pi3d.Texture("textures/mountains3_512.jpg")
 mymap = pi3d.ElevationMap("textures/mountainsHgt.jpg", name="map",
                      width=mapwidth, depth=mapdepth, height=mapheight,
                      ntiles=128, divx=32, divy=32) #testislands.jpg
-mymap.set_draw_details(shader, [clothimg, bumpimg, reflimg], 128.0, 0.0)
+mymap.set_draw_details(shader, [clothimg, bumpimg, reflimg], 128.0 * wv_width / 8.0, 0.0)
 mymap.set_fog(*FOG)
 
 #Create monument

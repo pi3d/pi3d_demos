1785a09971b6c28c6ebcb28a1e379f8b0c0cf8b6
admin <paddywwoof>
2015-03-23 11:19:04 +0000

    attempt to release curses afterwards

--- RunTests.py
+++ RunTests.py
@@ -17,8 +17,16 @@ test_list = ["Amazing", "Blur", "CastShadows", "ClashWalk", "ClothWalk",
         "Pi3d2", "Pi3d3", "PictureFrame", "Pong", "Post", "ProceduralTerrain",
         "RobotWalkabout", "Scenery", "Shapes", "Silo", "Slideshow_2d",
         "Slideshow_3d", "Slideshow", "Snake", "TigerShadow", "TigerTank", "Water"]
+
 for t in test_list:
   p = Popen([pyname, t + ".py"])
   print("\n\n{} >->".format(t))
-  time.sleep(2.5)
+  time.sleep(10.0)
   p.kill()
+
+import curses
+curses.nocbreak()
+key = curses.initscr()
+key.keypad(0)
+curses.echo()
+curses.endwin()

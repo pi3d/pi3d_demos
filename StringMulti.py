f37aff13427b8e1414fad8a086b171c711091144
admin <patrick@eldwick.org.uk>
2015-10-17 14:31:27 +0100

    Some tidying and bug fix for python3.2 v python3.3+ huh!

--- StringMulti.py
+++ StringMulti.py
@@ -63,49 +63,68 @@ pointFont = pi3d.Font(font_path, font_colour, codepoints=list(range(32,128)), fo
 text = pi3d.FastText(pointFont, CAMERA, max_chars=200)
 
 #Basic static text
-newtxt = pi3d.TextBlock(-100, -50, 0.1, 0.0, 14, None, None, text_format="Static string", size=0.9, spacing="F", space=0.05, colour=(0.0, 1.0, 0.0, 1.0) )
+newtxt = pi3d.TextBlock(-100, -50, 0.1, 0.0, 14, text_format="Static string",
+          size=0.9, spacing="F", space=0.05, colour=(0.0, 1.0, 0.0, 1.0) )
 text.add_text_block(newtxt)
 
-#The next three strings are formated with data from an object.  When the object data changes and the
-# text block is regenerated, the string is changed to the new data values.
-moving_text = pi3d.TextBlock(0, 0, 0.1, 0.0, 25, eg_object, "strA", text_format="{:s}", size=0.6, spacing="F", space=0.08, colour=(1.0, 0.0, 0.0, 1.0) )
+"""The next three strings are formated with data from an object.  When the
+object data changes and the text block is regenerated, the string is
+changed to the new data values."""
+moving_text = pi3d.TextBlock(0, 0, 0.1, 0.0, 25, eg_object, "strA",
+          text_format="{:s}", size=0.6, spacing="F", space=0.08,
+          colour=(1.0, 0.0, 0.0, 1.0) )
 text.add_text_block(moving_text)
 
-newtxt = pi3d.TextBlock(-150, 75, 0.1, 0.0, 18, eg_object, "valA", text_format="number: {:4.3f}", size=0.5, spacing="F", space=0.05, colour=(0.0, 0.0, 1.0, 1.0) )
+newtxt = pi3d.TextBlock(-150, 75, 0.1, 0.0, 18, eg_object, "valA",
+          text_format="number: {:4.3f}", size=0.5, spacing="F", space=0.05,
+          colour=(0.0, 0.0, 1.0, 1.0) )
 text.add_text_block(newtxt)
 
-newtxt = pi3d.TextBlock(200, -200, 0.1, 0.0, 10, eg_object, "fps", text_format="fps:{:2.1f}", size=0.75, spacing="C", space=0.6, colour=(0.0, 1.0, 1.0, 1.0) )
+newtxt = pi3d.TextBlock(200, -200, 0.1, 0.0, 10, eg_object, "fps",
+          text_format="fps:{:2.1f}", size=0.75, spacing="C", space=0.6,
+          colour=(0.0, 1.0, 1.0, 1.0) )
 text.add_text_block(newtxt)
 
 
 textSize = 0.1
-sizingText = pi3d.TextBlock(-100, 150, 0.1, 0.0, 15, None, None, text_format="Resizing text", size=textSize, spacing="F", space=0.05, colour=(1.0, 1.0, 0.0, 1.0) )
+sizingText = pi3d.TextBlock(-100, 150, 0.1, 0.0, 15,
+          text_format="Resizing text", size=textSize, spacing="F",
+          space=0.05, colour=(1.0, 1.0, 0.0, 1.0) )
 text.add_text_block(sizingText)
 
 # String rotation and spacing
 textRotation = 0.0
-rotatingText = pi3d.TextBlock(-200, -150, 0.1, textRotation, 15, None, None, text_format="Rotating text", size=0.7, spacing="C", space=0.6, colour=(1.0, 1.0, 1.0, 0.5), justify=0.5 )
+rotatingText = pi3d.TextBlock(-200, -150, 0.1, textRotation, 15,
+          text_format="Rotating text", size=0.7, spacing="C", space=0.6,
+          colour=(1.0, 1.0, 1.0, 0.5), justify=0.5 )
 text.add_text_block(rotatingText)
 
-rotatingChars = pi3d.TextBlock(-300, -100, 0.1, 0.0, 15, None, None, text_format="Rotating chars", size=0.6, spacing="C", space=0.6, colour=(0.99, 0.5, 0.5, 1.0) )
+rotatingChars = pi3d.TextBlock(-300, -100, 0.1, 0.0, 15,
+          text_format="Rotating chars", size=0.6, spacing="C", space=0.6,
+          colour=(0.99, 0.5, 0.5, 1.0) )
 text.add_text_block(rotatingChars)
 
-spacingText = pi3d.TextBlock(-350, -300, 0.1, 0.0, 10, None, None, text_format="Spacing", size=0.7, spacing="C", space=0.1, colour=(0.5, 1.0, 0.5, 1.0) )
+spacingText = pi3d.TextBlock(-350, -300, 0.1, 0.0, 10, text_format="Spacing",
+          size=0.7, spacing="C", space=0.1, colour=(0.5, 1.0, 0.5, 1.0) )
 text.add_text_block(spacingText)
 
 #String colour and alpha
 textAlpha = 0.1
-alphaText = pi3d.TextBlock(-250, -300, 0.0, 90.0, 15, None, None, text_format="Alpha change", size=0.99, spacing="C", space=0.6, colour=(0.99, 0.99, 0.99, textAlpha) )
+alphaText = pi3d.TextBlock(-250, -300, 0.0, 90.0, 15, text_format="Alpha change",
+          size=0.99, spacing="C", space=0.6, colour=(0.99, 0.99, 0.99, textAlpha) )
 text.add_text_block(alphaText)
 
-colourText = pi3d.TextBlock(-300, -200, 1.0, 0.0, 15, None, None, text_format="Colour change", size=0.8, spacing="C", space=0.6, colour=(0.99, 0.5, 0.5, 1.0) )
+colourText = pi3d.TextBlock(-300, -200, 1.0, 0.0, 15, text_format="Colour change",
+          size=0.8, spacing="C", space=0.6, colour=(0.99, 0.5, 0.5, 1.0) )
 text.add_text_block(colourText)
 
-gradientText = pi3d.TextBlock(50, -250, 0.1, 0.0, 16, None, None, text_format="Colour Gradient", size=0.7, spacing="C", space=0.6, colour=(0.5, 1.0, 0.5, 1.0) )
+gradientText = pi3d.TextBlock(50, -250, 0.1, 0.0, 16, text_format="Colour Gradient",
+          size=0.7, spacing="C", space=0.6, colour=(0.5, 1.0, 0.5, 1.0) )
 text.add_text_block(gradientText)
 gradientText.set_colour_gradient((1.0, 0.0, 0.0, 1.0), (0.0, 1.0, 0.0, 0.0), 1.0, 1.0)
 
-alphaGradientText = pi3d.TextBlock(50, -100, 0.1, 0.0, 16, None, None, text_format="Alpha Gradient", size=0.7, spacing="C", space=0.6, colour=(0.5, 1.0, 0.5, 1.0) )
+alphaGradientText = pi3d.TextBlock(50, -100, 0.1, 0.0, 16, text_format="Alpha Gradient",
+          size=0.7, spacing="C", space=0.6, colour=(0.5, 1.0, 0.5, 1.0) )
 text.add_text_block(alphaGradientText)
 alphaGradientText.set_colour_gradient((1.0, 1.0, 1.0, 1.0), (1.0, 1.0, 1.0, 0.0), 1.0, 0.25)
 

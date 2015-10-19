c9dbf2c2112247412bc3a6f3b4e2d4b3b0ed0fc3
admin <patrick@eldwick.org.uk>
2015-10-19 23:01:42 +0100

    further improvements to PointText from matt

--- StringMulti.py
+++ StringMulti.py
@@ -5,7 +5,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 Font - For generating a font drawn with gl_point and recoloured using
  shader uv_pointsprite
 
-FastText - Manages the collection of data for all of the strings to be drawn.
+PointText - Manages the collection of data for all of the strings to be drawn.
   Enables drawing all strings in one pass.
   
 TextBlock - A string which has its content, position, size, rotation, colour, alpha,
@@ -29,14 +29,15 @@ import time
 
 
 """ An example class to contain some data to display, showing how the
-FastText class can be be passed object and attributes rather than plain
+PointText class can be be passed object and attributes rather than plain
 text. If the text represented by the attribute changes then the block will
-be regenerated when FastText.regen() is called. 
+be regenerated when PointText.regen() is called. 
 """
 class EgClass(object):
   valA = 0.0
   fps = 0.0
   strA = "A moving string"
+  angle = 0.0
 
 eg_object = EgClass() # create an instance of the example class
 
@@ -51,7 +52,7 @@ QWIDTH = HWIDTH/2 # quarter width
 
 CAMERA = pi3d.Camera(is_3d=False)
 
-font_colour = (255,255,255,255)
+font_colour = (255, 255, 255, 255)
 
 text_pos = QWIDTH
 
@@ -60,7 +61,7 @@ font_path = os.path.abspath(os.path.join(working_directory, 'fonts', 'FreeSans.t
 
 # Create pointFont and the text manager to use it
 pointFont = pi3d.Font(font_path, font_colour, codepoints=list(range(32,128)), font_size=48)
-text = pi3d.FastText(pointFont, CAMERA, max_chars=200)
+text = pi3d.PointText(pointFont, CAMERA, max_chars=200)
 
 #Basic static text
 newtxt = pi3d.TextBlock(-100, -50, 0.1, 0.0, 14, text_format="Static string",
@@ -70,23 +71,24 @@ text.add_text_block(newtxt)
 """The next three strings are formated with data from an object.  When the
 object data changes and the text block is regenerated, the string is
 changed to the new data values."""
-moving_text = pi3d.TextBlock(0, 0, 0.1, 0.0, 25, eg_object, "strA",
+moving_text = pi3d.TextBlock(0, 0, 0.1, 0.0, 25, data_obj=eg_object, attr="strA",
           text_format="{:s}", size=0.6, spacing="F", space=0.08,
           colour=(1.0, 0.0, 0.0, 1.0) )
 text.add_text_block(moving_text)
 
-newtxt = pi3d.TextBlock(-150, 75, 0.1, 0.0, 18, eg_object, "valA",
+newtxt = pi3d.TextBlock(-150, 75, 0.1, 0.0, 18, data_obj=eg_object, attr="valA",
           text_format="number: {:4.3f}", size=0.5, spacing="F", space=0.05,
           colour=(0.0, 0.0, 1.0, 1.0) )
 text.add_text_block(newtxt)
 
-newtxt = pi3d.TextBlock(200, -200, 0.1, 0.0, 10, eg_object, "fps",
+newtxt = pi3d.TextBlock(200, -200, 0.1, 0.0, 10, data_obj=eg_object, attr="fps",
           text_format="fps:{:2.1f}", size=0.75, spacing="C", space=0.6,
           colour=(0.0, 1.0, 1.0, 1.0) )
 text.add_text_block(newtxt)
 
 
 textSize = 0.1
+textGrowth=0.01
 sizingText = pi3d.TextBlock(-100, 150, 0.1, 0.0, 15,
           text_format="Resizing text", size=textSize, spacing="F",
           space=0.05, colour=(1.0, 1.0, 0.0, 1.0) )
@@ -110,6 +112,7 @@ text.add_text_block(spacingText)
 
 #String colour and alpha
 textAlpha = 0.1
+alphaGrowth = 0.01
 alphaText = pi3d.TextBlock(-250, -300, 0.0, 90.0, 15, text_format="Alpha change",
           size=0.99, spacing="C", space=0.6, colour=(0.99, 0.99, 0.99, textAlpha) )
 text.add_text_block(alphaText)
@@ -118,15 +121,25 @@ colourText = pi3d.TextBlock(-300, -200, 1.0, 0.0, 15, text_format="Colour change
           size=0.8, spacing="C", space=0.6, colour=(0.99, 0.5, 0.5, 1.0) )
 text.add_text_block(colourText)
 
+colourGradient = pi3d.TextBlockColourGradient((1.0,0.0,0.0,1.0), (0.0,1.0,0.0,1.0))
 gradientText = pi3d.TextBlock(50, -250, 0.1, 0.0, 16, text_format="Colour Gradient",
-          size=0.7, spacing="C", space=0.6, colour=(0.5, 1.0, 0.5, 1.0) )
+          size=0.7, spacing="C", space=0.6, colour=colourGradient)
 text.add_text_block(gradientText)
-gradientText.set_colour_gradient((1.0, 0.0, 0.0, 1.0), (0.0, 1.0, 0.0, 0.0), 1.0, 1.0)
 
+alphaGradient = pi3d.TextBlockColourGradient((1.0,1.0,1.0,1.0), (1.0,1.0,1.0,0.25))
 alphaGradientText = pi3d.TextBlock(50, -100, 0.1, 0.0, 16, text_format="Alpha Gradient",
-          size=0.7, spacing="C", space=0.6, colour=(0.5, 1.0, 0.5, 1.0) )
+          size=0.7, spacing="C", space=0.6, colour=alphaGradient)
 text.add_text_block(alphaGradientText)
-alphaGradientText.set_colour_gradient((1.0, 1.0, 1.0, 1.0), (1.0, 1.0, 1.0, 0.0), 1.0, 0.25)
+
+everythingColour = pi3d.TextBlockColourGradient((1-textAlpha,0.0,textAlpha,1.0),
+          (textAlpha,0.0,1-textAlpha,1.0))
+everythingText = pi3d.TextBlock(100, 200, 0.1, textRotation, 15, data_obj=eg_object,
+          attr="angle", text_format="Angle: {:3.0f}", size=0.7, spacing="F", space=0.05,
+          colour=everythingColour, justify=0.5 )
+#everythingText = TextBlock(100, 200, 0.1, textRotation, 15, data_obj=eg_object,
+#          attr="angle", text_format="Angle: {:2.3f}", size=0.7, spacing="C", space=0.6,
+#colour=(1.0, 1.0, 1.0, 0.5), justify=0.5 )
+text.add_text_block(everythingText)
 
 frame_count = 0
 end_time = time.time() + 1.0
@@ -140,34 +153,42 @@ while DISPLAY.loop_running():
   eg_object.valA *= 1.0123
   if eg_object.valA > 1000.0:
     eg_object.valA *= 0.01
-    
+
   textSize += 0.01
   if textSize > 0.99:
     textSize = 0.1
- 
+
   sizingText.set_text(size=textSize)
   
-  textRotation += 1.0
-  if textRotation >= 180.0:
-    textRotation -= 360.0
-    
+  textRotation = (textRotation + 181.0) % 360.0 - 180.0
+
+  eg_object.angle = textRotation
+
   rotatingText.set_position(rot=textRotation)
   rotatingChars.set_text(char_rot=textRotation)
-  
-  textAlpha += 0.01
+
+  textAlpha += alphaGrowth
   if textAlpha >= 1.0:
-    textAlpha = 0.1
-  alphaText.set_colour(alpha=textAlpha)
+    textAlpha = 1.0
+    alphaGrowth = -0.01
+  elif textAlpha < 0.0:
+    textAlpha = 0.0
+    alphaGrowth = 0.02
+  alphaText.colouring.set_colour(alpha=textAlpha)
   
   colour_angle = math.radians(textRotation)
   red = math.cos(colour_angle)
   blue = math.cos(colour_angle + (math.pi * 0.666) )  
   green = math.cos(colour_angle + (math.pi * 1.333) )  
   colour = [red, green , blue, 1.0]
-  colourText.set_colour(colour)
-  
+  colourText.colouring.set_colour(colour)
+
   spacingText.set_text(space=textSize)
-  
+
+  everythingText.set_text(space=textSize*0.25, set_pos=False, set_colour=False)
+  everythingText.set_position(x=text_pos-50, y=150+(text_pos*0.2), rot=textRotation)
+  everythingColour.set_colour( (1.0, textAlpha , 0.0, 1.0), (0.0, textAlpha, 1.0, 1.0) )
+
   now = time.time()
   frame_count += 1
   if now > end_time:
@@ -181,7 +202,6 @@ while DISPLAY.loop_running():
 
   text.regen()
   text.draw()
-  
 
   k = KEYBOARD.read()
   if k > -1:

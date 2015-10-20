545c19e4803a152ee0866bf2ac2a705bd98b2abc
admin <patrick@eldwick.org.uk>
2015-10-20 23:15:29 +0100

    fixed corner clipping or overlap with certain fonts

--- StringMulti.py
+++ StringMulti.py
@@ -60,12 +60,12 @@ working_directory = os.path.dirname(os.path.realpath(__file__))
 font_path = os.path.abspath(os.path.join(working_directory, 'fonts', 'FreeSans.ttf'))
 
 # Create pointFont and the text manager to use it
-pointFont = pi3d.Font(font_path, font_colour, codepoints=list(range(32,128)), font_size=48)
-text = pi3d.PointText(pointFont, CAMERA, max_chars=200)
+pointFont = pi3d.Font(font_path, font_colour, codepoints=list(range(32,128)))
+text = pi3d.PointText(pointFont, CAMERA, max_chars=200, point_size=64)
 
 #Basic static text
 newtxt = pi3d.TextBlock(-100, -50, 0.1, 0.0, 14, text_format="Static string",
-          size=0.9, spacing="F", space=0.05, colour=(0.0, 1.0, 0.0, 1.0) )
+          size=0.99, spacing="F", space=0.05, colour=(0.0, 1.0, 0.0, 1.0))
 text.add_text_block(newtxt)
 
 """The next three strings are formated with data from an object.  When the
@@ -73,17 +73,17 @@ object data changes and the text block is regenerated, the string is
 changed to the new data values."""
 moving_text = pi3d.TextBlock(0, 0, 0.1, 0.0, 25, data_obj=eg_object, attr="strA",
           text_format="{:s}", size=0.6, spacing="F", space=0.08,
-          colour=(1.0, 0.0, 0.0, 1.0) )
+          colour=(1.0, 0.0, 0.0, 1.0))
 text.add_text_block(moving_text)
 
 newtxt = pi3d.TextBlock(-150, 75, 0.1, 0.0, 18, data_obj=eg_object, attr="valA",
           text_format="number: {:4.3f}", size=0.5, spacing="F", space=0.05,
-          colour=(0.0, 0.0, 1.0, 1.0) )
+          colour=(0.0, 0.0, 1.0, 1.0))
 text.add_text_block(newtxt)
 
 newtxt = pi3d.TextBlock(200, -200, 0.1, 0.0, 10, data_obj=eg_object, attr="fps",
           text_format="fps:{:2.1f}", size=0.75, spacing="C", space=0.6,
-          colour=(0.0, 1.0, 1.0, 1.0) )
+          colour=(0.0, 1.0, 1.0, 1.0))
 text.add_text_block(newtxt)
 
 
@@ -91,11 +91,11 @@ textSize = 0.1
 textGrowth=0.01
 sizingText = pi3d.TextBlock(-100, 150, 0.1, 0.0, 15,
           text_format="Resizing text", size=textSize, spacing="F",
-          space=0.05, colour=(1.0, 1.0, 0.0, 1.0) )
+          space=0.05, colour=(1.0, 1.0, 0.0, 1.0))
 text.add_text_block(sizingText)
 
 # String rotation and spacing
-textRotation = 0.0
+textRotation = 30.0
 rotatingText = pi3d.TextBlock(-200, -150, 0.1, textRotation, 15,
           text_format="Rotating text", size=0.7, spacing="C", space=0.6,
           colour=(1.0, 1.0, 1.0, 0.5), justify=0.5 )
@@ -103,22 +103,22 @@ text.add_text_block(rotatingText)
 
 rotatingChars = pi3d.TextBlock(-300, -100, 0.1, 0.0, 15,
           text_format="Rotating chars", size=0.6, spacing="C", space=0.6,
-          colour=(0.99, 0.5, 0.5, 1.0) )
+          colour=(0.99, 0.5, 0.5, 1.0))
 text.add_text_block(rotatingChars)
 
 spacingText = pi3d.TextBlock(-350, -300, 0.1, 0.0, 10, text_format="Spacing",
-          size=0.7, spacing="C", space=0.1, colour=(0.5, 1.0, 0.5, 1.0) )
+          size=0.7, spacing="C", space=0.1, colour=(0.5, 1.0, 0.5, 1.0))
 text.add_text_block(spacingText)
 
 #String colour and alpha
 textAlpha = 0.1
 alphaGrowth = 0.01
 alphaText = pi3d.TextBlock(-250, -300, 0.0, 90.0, 15, text_format="Alpha change",
-          size=0.99, spacing="C", space=0.6, colour=(0.99, 0.99, 0.99, textAlpha) )
+          size=0.99, spacing="C", space=0.6, colour=(0.99, 0.99, 0.99, textAlpha))
 text.add_text_block(alphaText)
 
 colourText = pi3d.TextBlock(-300, -200, 1.0, 0.0, 15, text_format="Colour change",
-          size=0.8, spacing="C", space=0.6, colour=(0.99, 0.5, 0.5, 1.0) )
+          size=0.8, spacing="C", space=0.6, colour=(0.99, 0.5, 0.5, 1.0))
 text.add_text_block(colourText)
 
 colourGradient = pi3d.TextBlockColourGradient((1.0,0.0,0.0,1.0), (0.0,1.0,0.0,1.0))
@@ -178,8 +178,8 @@ while DISPLAY.loop_running():
   
   colour_angle = math.radians(textRotation)
   red = math.cos(colour_angle)
-  blue = math.cos(colour_angle + (math.pi * 0.666) )  
-  green = math.cos(colour_angle + (math.pi * 1.333) )  
+  blue = math.cos(colour_angle + (math.pi * 0.666))  
+  green = math.cos(colour_angle + (math.pi * 1.333))  
   colour = [red, green , blue, 1.0]
   colourText.colouring.set_colour(colour)
 
@@ -187,7 +187,7 @@ while DISPLAY.loop_running():
 
   everythingText.set_text(space=textSize*0.25, set_pos=False, set_colour=False)
   everythingText.set_position(x=text_pos-50, y=150+(text_pos*0.2), rot=textRotation)
-  everythingColour.set_colour( (1.0, textAlpha , 0.0, 1.0), (0.0, textAlpha, 1.0, 1.0) )
+  everythingColour.set_colour( (1.0, textAlpha , 0.0, 1.0), (0.0, textAlpha, 1.0, 1.0))
 
   now = time.time()
   frame_count += 1

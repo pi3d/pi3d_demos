bbdf2a134784d8e8b5510344b80e0604606220ac
paddywwoof <patrick@eldwick.org.uk>
2019-11-13 23:32:17 +0000

    PictureFrame with rotation using exif and reshuffle if images added to directory

--- PictureFrame.py
+++ PictureFrame.py
@@ -8,6 +8,8 @@ and reduced grid_size to give better resolution for large characters.
 see http://pi3d.github.io/html/FAQ.html and https://www.thedigitalpictureframe.com/control-your-digital-picture-frame-with-home-assistents-wifi-presence-detection-and-mqtt/
 and https://www.cloudmqtt.com/plans.html
 
+USING exif info to rotate images
+
     ESC to quit, 's' to reverse, any other key to move on one.
 '''
 import os
@@ -22,15 +24,19 @@ from PIL import Image, ExifTags # these are needed for getting exif data from im
 # these variables are constants
 #####################################################
 PIC_DIR = '/home/pi/pi3d_demos/textures' #'textures'
-#PIC_DIR = '/home/patrick/python/pi3d_demos/textures/temp' #'textures'
+#PIC_DIR = '/home/patrick/python/pi3d_demos/textures' #'textures'
 FPS = 20
 FIT = True
 EDGE_ALPHA = 0.0 # see background colour at edge. 1.0 would show reflection of image
 BACKGROUND = (0.2, 0.2, 0.2, 1.0)
 RESHUFFLE_NUM = 5 # times through before reshuffling
-FONT_FILE = 'fonts/NotoSans-Regular.ttf'
+FONT_FILE = '/home/pi/pi3d_demos/fonts/NotoSans-Regular.ttf'
+#FONT_FILE = '/home/patrick/python/pi3d_demos/fonts/NotoSans-Regular.ttf'
 CODEPOINTS = '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ., _-/' # limit to 49 ie 7x7 grid_size
-USE_MQTT = False
+USE_MQTT = True
+RECENT_N = 4 # shuffle the most recent ones to play before the rest
+SHOW_NAMES = False
+CHECK_DIR_TM = 60.0 # seconds to wait between checking if directory has changed
 #####################################################
 # these variables can be altered using mqtt messaging
 #####################################################
@@ -43,16 +49,32 @@ quit = False
 paused = False # NB must be set to True after the first iteration of the show!
 
 delta_alpha = 1.0 / (FPS * fade_time) # delta alpha
+last_file_change = 0.0 # holds last change time in directory structure
+next_check_tm = time.time() + CHECK_DIR_TM # check if new file or directory every hour
 
 #####################################################
 # some functions to tidy subsequent code
 #####################################################
-def tex_load(fname):
+def tex_load(fname, orientation):
   try:
-    tex = pi3d.Texture(fname, blend=True, m_repeat=True)
+    im = Image.open(fname)
+    if orientation == 2:
+        im = im.transpose(Image.FLIP_LEFT_RIGHT)
+    if orientation == 3:
+        im = im.transpose(Image.ROTATE_180) # rotations are clockwise
+    if orientation == 4:
+        im = im.transpose(Image.FLIP_TOP_BOTTOM)
+    if orientation == 5:
+        im = im.transpose(Image.FLIP_LEFT_RIGHT).transpose(Image.ROTATE_270)
+    if orientation == 6:
+        im = im.transpose(Image.ROTATE_270)
+    if orientation == 7:
+        im = im.transpose(Image.FLIP_LEFT_RIGHT).transpose(Image.ROTATE_90)
+    if orientation == 8:
+        im = im.transpose(Image.ROTATE_90)
+    tex = pi3d.Texture(im, blend=True, m_repeat=True)
   except Exception as e:
-    print('''Couldn't load file {}
-            (probably related to Apple and or Adobe making assumptions about hardware!!): {}'''.format(fname, e))
+    print('''Couldn't load file {} giving error: {}'''.format(fname, e))
     tex = None
   return tex
 
@@ -61,45 +83,70 @@ def tidy_name(path_name):
     name = ''.join([c for c in name if c in CODEPOINTS])
     return name
 
+def check_changes():
+  global last_file_change
+  update = False
+  for root, _, _ in os.walk(PIC_DIR):
+      mod_tm = os.stat(root).st_mtime
+      if mod_tm > last_file_change:
+        last_file_change = mod_tm
+        update = True
+  return update
+
 def get_files(dt_from=None, dt_to=None):
   # dt_from and dt_to are either None or tuples (2016,12,25)
   if dt_from is not None:
     dt_from = time.mktime(dt_from + (0, 0, 0, 0, 0, 0))
   if dt_to is not None:
     dt_to = time.mktime(dt_to + (0, 0, 0, 0, 0, 0))
-  global shuffle, PIC_DIR, EXIF_DATID
+  global shuffle, PIC_DIR, EXIF_DATID, last_file_change
   file_list = []
   extensions = ['.png','.jpg','.jpeg'] # can add to these
   for root, _dirnames, filenames in os.walk(PIC_DIR):
+      mod_tm = os.stat(root).st_mtime # time of alteration in a directory
+      if mod_tm > last_file_change:
+        last_file_change = mod_tm
       for filename in filenames:
           ext = os.path.splitext(filename)[1].lower()
           if ext in extensions and not '.AppleDouble' in root and not filename.startswith('.'):
               file_path_name = os.path.join(root, filename)
               include_flag = True
-              if EXIF_DATID is not None and (dt_from is not None or dt_to is not None):
+              orientation = 1 # this is default - unrotated
+              if EXIF_DATID is not None and EXIF_ORIENTATION is not None:
                 try:
                   im = Image.open(file_path_name) # lazy operation so shouldn't load (better test though)
                   #print(filename, end="")
+                  exif_data = im._getexif()
+                  #print('orientation is {}'.format(exif_data[EXIF_ORIENTATION]))
                   dt = time.mktime(
-                        time.strptime(im._getexif()[EXIF_DATID], '%Y:%m:%d %H:%M:%S'))
+                        time.strptime(exif_data[EXIF_DATID], '%Y:%m:%d %H:%M:%S'))
+                  orientation = int(exif_data[EXIF_ORIENTATION])
                 except Exception as e: # NB should really check error here but it's almost certainly due to lack of exif data
-                  print(e)
+                  print('trying to read exif', e)
                   dt = os.path.getmtime(file_path_name) # so use file last modified date
                 if (dt_from is not None and dt < dt_from) or (dt_to is not None and dt > dt_to):
                   include_flag = False
               if include_flag:
-                file_list.append(file_path_name) 
+                file_list.append((file_path_name, orientation, os.path.getmtime(file_path_name))) # iFiles now list of tuples (file_name, orientation) 
   if shuffle:
-    random.shuffle(file_list) # randomize pictures
+    file_list.sort(key=lambda x: x[2]) # will be later files last
+    temp_list_first = file_list[-RECENT_N:]
+    temp_list_last = file_list[:-RECENT_N]
+    random.shuffle(temp_list_first)
+    random.shuffle(temp_list_last)
+    file_list = temp_list_first + temp_list_last
   else:
     file_list.sort() # if not suffled; sort by name
   return file_list, len(file_list) # tuple of file list, number of pictures
 
 EXIF_DATID = None # this needs to be set before get_files() above can extract exif date info
+EXIF_ORIENTATION = None
 for k in ExifTags.TAGS:
   if ExifTags.TAGS[k] == 'DateTimeOriginal':
     EXIF_DATID = k
-    break
+  if ExifTags.TAGS[k] == 'Orientation':
+    EXIF_ORIENTATION = k
+
 
 ##############################################
 # MQTT functionality - see https://www.thedigitalpictureframe.com/
@@ -164,10 +211,11 @@ if USE_MQTT:
     print("MQTT not set up because of: {}".format(e))
 ##############################################
 
-DISPLAY = pi3d.Display.create(x=0,y=0,frames_per_second=FPS, background=BACKGROUND)
+DISPLAY = pi3d.Display.create(x=0, y=0, frames_per_second=FPS, background=BACKGROUND)
 CAMERA = pi3d.Camera(is_3d=False)
 
-shader = pi3d.Shader("shaders/blend_new")
+shader = pi3d.Shader("/home/pi/pi3d_demos/shaders/blend_new")
+#shader = pi3d.Shader("/home/patrick/python/pi3d_demos/shaders/blend_new")
 slide = pi3d.Sprite(camera=CAMERA, w=DISPLAY.width, h=DISPLAY.height, z=5.0)
 slide.set_shader(shader)
 slide.unif[47] = EDGE_ALPHA
@@ -184,14 +232,14 @@ if nFi == 0:
   print('No files selected!')
   exit()
 
-# PointText and TextBlock
+# PointText and TextBlock. If SHOW_NAMES is False then this is just used for no images message
 font = pi3d.Font(FONT_FILE, codepoints=CODEPOINTS, grid_size=7, shadow_radius=4.0,
-                 shadow=(0,0,0,128))
+                shadow=(0,0,0,128))
 text = pi3d.PointText(font, CAMERA, max_chars=200, point_size=50)
 textblock = pi3d.TextBlock(x=-DISPLAY.width * 0.5 + 50, y=-DISPLAY.height * 0.4,
-                           z=0.1, rot=0.0, char_count=199,
-                           text_format="{}".format(tidy_name(iFiles[next_pic_num])), size=0.99, 
-                           spacing="F", space=0.02, colour=(1.0, 1.0, 1.0, 1.0))
+                          z=0.1, rot=0.0, char_count=199,
+                          text_format="{}".format(" "), size=0.99, 
+                          spacing="F", space=0.02, colour=(1.0, 1.0, 1.0, 1.0))
 text.add_text_block(textblock)
 
 num_run_through = 0
@@ -205,7 +253,7 @@ while DISPLAY.loop_running():
       sfg = None
       while sfg is None: # keep going through until a usable picture is found TODO break out how?
         pic_num = next_pic_num
-        sfg = tex_load(iFiles[pic_num])
+        sfg = tex_load(iFiles[pic_num][0], iFiles[pic_num][1])
         next_pic_num += 1
         if next_pic_num >= nFi:
           num_run_through += 1
@@ -229,15 +277,24 @@ while DISPLAY.loop_running():
       slide.unif[os1] = (wh_rat - 1.0) * 0.5
       slide.unif[os2] = 0.0
       # set the file name as the description
-      textblock.set_text(text_format="{}".format(tidy_name(iFiles[pic_num])))
-      text.regen()
+      if SHOW_NAMES:
+        textblock.set_text(text_format="{}".format(tidy_name(iFiles[pic_num][0])))
+        text.regen()
 
-    if a < 1.0:
+    if a < 1.0: # transition is happening
       a += delta_alpha
       slide.unif[44] = a
-      # this sets alpha for the TextBlock from 0 to 1 then back to 0
-      textblock.colouring.set_colour(alpha=(1.0 - abs(1.0 - 2.0 * a)))
-      text.regen()
+      if SHOW_NAMES:
+        # this sets alpha for the TextBlock from 0 to 1 then back to 0
+        textblock.colouring.set_colour(alpha=(1.0 - abs(1.0 - 2.0 * a)))
+        text.regen()
+    else: # no transition effect safe to resuffle etc
+      if tm > next_check_tm:
+        if check_changes():
+          iFiles, nFi = get_files(date_from, date_to)
+          num_run_through = 0
+          next_pic_num = 0
+        next_check_tm = tm + CHECK_DIR_TM # once per hour
 
     slide.draw()
 
--- demo.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.path.insert(1, '/home/pi/pi3d')
-
